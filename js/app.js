d95f3ed6c84ffc2e4df5de118b6f179765bdd466
Jordi Boronat <jorx.bro@gmail.com>
2020-11-04 16:43:01 +0100

    Efecto profundidad

--- js/app.js
+++ js/app.js
@@ -9,15 +9,21 @@ const OffTheLineClone = {
     hero: undefined,
     key: " ",
     score: 0,
-    timer: 30,
+    timer: 15,
     coins: [],
     canvasSize: {
         w: 700,
         h: 500
     },
-    guideLine: {
-        x: undefined,
-        y: undefined
+    limitSquare: {
+        pos: {
+            x: undefined,
+            y: undefined
+        },
+        size: {
+            w: undefined,
+            h: undefined
+        }
     },
     interval: undefined,
 
@@ -25,12 +31,14 @@ const OffTheLineClone = {
         this.canvasTag = document.getElementById(id)
         this.ctx = this.canvasTag.getContext('2d')
         this.setDimensions()
-        this.guideLineInsert()
         this.createHero()
         this.setListener()
         this.drawAll()
         this.setTimer()
         this.createCoins()
+        this.limitSquareData()
+
+        console.log(this.limitSquare)
 
     },
 
@@ -41,11 +49,6 @@ const OffTheLineClone = {
         this.canvasTag.setAttribute('height', this.canvasSize.h)
     },
 
-    guideLineInsert() {
-        this.guideLine.x = this.canvasSize.w / 2 - 125
-        this.guideLine.y = this.canvasSize.h / 2 - 125
-    },
-    
     drawStart() {
         this.drawBackground()
         this.ctx.font = " 20px verdana, sans-serif"
@@ -59,25 +62,31 @@ const OffTheLineClone = {
             }
         }
     },
-    
+
 
     drawAll() {
+
         setInterval(() => {
-            this.clearScreen()
-            this.drawBackground()
-            //this.matrixDraw()
-            this.drawLinearSquare()
-            //this.drawCoinsGuide()
-            this.hero.moveHero()
-            this.drawCoins()
-            this.drawText()
-            this.drawTimer()
-            this.stopTimer()
-            this.collisionDetector()
-            this.hero.drawHero()
+            if (this.timer >= 0) {
+                this.clearScreen()
+                this.drawBackground()
+                //this.matrixDraw()
+                this.drawLinearSquare()
+                //this.drawLimitSquare()
+                this.drawFrame()
+                //this.drawCoinsGuide()
+                this.hero.moveHero()
+                this.drawCoins()
+                this.drawText()
+                this.drawTimer()
+                this.stopTimer()
+                this.collisionDetector()
+                this.hero.drawHero()
+            } else {
+                this.drawGameOver()
+            }
 
-
-        }, 1000 / 60)
+        }, 5)
 
     },
 
@@ -89,22 +98,72 @@ const OffTheLineClone = {
     drawLinearSquare() {
         //this.ctx.filter = 'blur(2px)'
         this.ctx.lineWidth = 2
-        this.ctx.strokeStyle = '#EB8FAA'
-        this.ctx.strokeRect(this.canvasSize.w / 2 - 125, this.canvasSize.h / 2 - 125, 250, 250)
+        let alpha = 0.8
+        let width = 250
+        let height = 250
+
+        this.ctx.strokeStyle = `rgb(235, 143, 170)`
+        this.ctx.strokeRect(this.canvasSize.w / 2 - (125), this.canvasSize.h / 2 - (125), 250, 250)
+
+        for (let i = 0; i < 10; i++) {
+            this.ctx.strokeStyle = `rgba(235, 143, 170, ${alpha})`
+            this.ctx.strokeRect(this.canvasSize.w / 2 - (width / 2), this.canvasSize.h / 2 - (width / 2), width, height)
+            alpha -= 0.1
+            width -= 16
+            height -= 16
+        }
+
     },
+
+    drawLimitSquare() {
+        //this.ctx.filter = 'blur(2px)'
+        this.ctx.lineWidth = 2
+        this.ctx.strokeStyle = 'red'
+        this.ctx.strokeRect(this.canvasSize.w / 2 - 135, this.canvasSize.h / 2 - 135, 270, 270)
+    },
+
+    limitSquareData() {
+        this.limitSquare.pos.x = this.canvasSize.w / 2 - 135
+        this.limitSquare.pos.y = this.canvasSize.h / 2 - 135
+        this.limitSquare.size.w = 270
+        this.limitSquare.size.h = 270
+    },
+
     drawCoinsGuide() {
         this.ctx.lineWidth = 2
         this.ctx.strokeStyle = 'white'
         this.ctx.strokeRect(this.canvasSize.w / 2 - 75, this.canvasSize.h / 2 - 75, 150, 150)
     },
 
+    drawFrame() {
+        this.ctx.lineWidth = 2
+        this.ctx.strokeStyle = '#77EBC7'
+        this.ctx.strokeRect(this.canvasSize.w / 2 - 275, this.canvasSize.h / 2 - 225, 550, 450)
+    },
+
+    drawGameOver() {
+        this.ctx.fillStyle = 'black'
+        this.ctx.fillRect(this.canvasSize.w / 2 - (this.canvasSize.w / 2), this.canvasSize.h / 2 - (this.canvasSize.h / 2), this.canvasSize.w, this.canvasSize.h)
+        this.ctx.font = "100 30px roboto, sans-serif"
+        this.ctx.textAlign = "start"
+        this.ctx.textBaseLine = "bottom"
+        this.ctx.fillStyle = "white"
+        this.ctx.fillText(`TIME IS OVER`, this.canvasSize.w / 2 - 90, this.canvasSize.h / 2 - 40)
+
+        this.ctx.font = "100 20px roboto, sans-serif"
+        this.ctx.textAlign = "start"
+        this.ctx.textBaseLine = "bottom"
+        this.ctx.fillStyle = "yellow"
+        this.ctx.fillText(`Score: ${this.score}`, this.canvasSize.w / 2 - 35, this.canvasSize.h / 2 - 10)
+    },
+
     drawCoins() {
 
         this.coins.forEach(elm => {
             elm.draw()
-            
+
         })
-      
+
     },
 
     matrixDraw() {
@@ -146,20 +205,20 @@ const OffTheLineClone = {
     },
 
     drawText() {
-        this.ctx.font = " 20px verdana, sans-serif"
+        this.ctx.font = "100 23px roboto, sans-serif"
         this.ctx.textAlign = "start"
         this.ctx.textBaseLine = "bottom"
         this.ctx.fillStyle = "yellow"
-        this.ctx.fillText(`Score: ${this.score}`, 100, 50)
+        this.ctx.fillText(`Score: ${this.score}`, 90, 50)
 
     },
 
     drawTimer() {
-        this.ctx.font = " 20px verdana, sans-serif"
+        this.ctx.font = "100 23px roboto, sans-serif"
         this.ctx.textAlign = "start"
         this.ctx.textBaseLine = "bottom"
         this.ctx.fillStyle = "yellow"
-        this.ctx.fillText(`Time: ${this.timer}`, 500, 50)
+        this.ctx.fillText(`Time: ${this.timer}`, 525, 50)
     },
 
     setTimer() {
@@ -177,34 +236,34 @@ const OffTheLineClone = {
 
 
     createHero() {
-        this.hero = new Hero(this.ctx, this.canvasSize, this.guideLine, 20, 20, 1)
-        // this.hero = new Hero(this.ctx, this.canvasSize, 100, 100, 100, 100)
+        this.hero = new Hero(this.ctx, this.canvasSize, this.limitSquare, 20, 20, 1)
+
     },
 
     createCoins() {
 
         //Línea 1
-        let coin1 = new Coins(this.ctx, this.canvasSize.w / 2 - 85, this.canvasSize.h / 2 - 85, 20, 20)
-        let coin2 = new Coins(this.ctx, this.canvasSize.w / 2 - 35, this.canvasSize.h / 2 - 85, 20, 20)
-        let coin3 = new Coins(this.ctx, this.canvasSize.w / 2 + 15, this.canvasSize.h / 2 - 85, 20, 20)
-        let coin4 = new Coins(this.ctx, this.canvasSize.w / 2 + 65, this.canvasSize.h / 2 - 85, 20, 20)
-         //Línea 2
-        let coin5 = new Coins(this.ctx, this.canvasSize.w / 2 - 85, this.canvasSize.h / 2 - 35, 20, 20)
-        let coin6 = new Coins(this.ctx, this.canvasSize.w / 2 - 35, this.canvasSize.h / 2 - 35, 20, 20)
-        let coin7 = new Coins(this.ctx, this.canvasSize.w / 2 + 15, this.canvasSize.h / 2 - 35, 20, 20)
-        let coin8 = new Coins(this.ctx, this.canvasSize.w / 2 + 65, this.canvasSize.h / 2 - 35, 20, 20)
+        let coin1 = new Coins(this.ctx, this.canvasSize.w / 2 - 80, this.canvasSize.h / 2 - 80, 10, 10)
+        let coin2 = new Coins(this.ctx, this.canvasSize.w / 2 - 30, this.canvasSize.h / 2 - 80, 10, 10)
+        let coin3 = new Coins(this.ctx, this.canvasSize.w / 2 + 20, this.canvasSize.h / 2 - 80, 10, 10)
+        let coin4 = new Coins(this.ctx, this.canvasSize.w / 2 + 70, this.canvasSize.h / 2 - 80, 10, 10)
+        //Línea 2
+        let coin5 = new Coins(this.ctx, this.canvasSize.w / 2 - 80, this.canvasSize.h / 2 - 30, 10, 10)
+        let coin6 = new Coins(this.ctx, this.canvasSize.w / 2 - 30, this.canvasSize.h / 2 - 30, 10, 10)
+        let coin7 = new Coins(this.ctx, this.canvasSize.w / 2 + 20, this.canvasSize.h / 2 - 30, 10, 10)
+        let coin8 = new Coins(this.ctx, this.canvasSize.w / 2 + 70, this.canvasSize.h / 2 - 30, 10, 10)
 
-         //Línea 3
-        let coin9 = new Coins(this.ctx, this.canvasSize.w / 2 - 85, this.canvasSize.h / 2 + 15, 20, 20)
-        let coin10 = new Coins(this.ctx, this.canvasSize.w / 2 - 35, this.canvasSize.h / 2 + 15, 20, 20)
-        let coin11 = new Coins(this.ctx, this.canvasSize.w / 2 + 15, this.canvasSize.h / 2 + 15, 20, 20)
-        let coin12 = new Coins(this.ctx, this.canvasSize.w / 2 + 65, this.canvasSize.h / 2 + 15, 20, 20)
+        //Línea 3
+        let coin9 = new Coins(this.ctx, this.canvasSize.w / 2 - 80, this.canvasSize.h / 2 + 20, 10, 10)
+        let coin10 = new Coins(this.ctx, this.canvasSize.w / 2 - 30, this.canvasSize.h / 2 + 20, 10, 10)
+        let coin11 = new Coins(this.ctx, this.canvasSize.w / 2 + 20, this.canvasSize.h / 2 + 20, 10, 10)
+        let coin12 = new Coins(this.ctx, this.canvasSize.w / 2 + 70, this.canvasSize.h / 2 + 20, 10, 10)
 
         //Línea 4
-        let coin13 = new Coins(this.ctx, this.canvasSize.w / 2 - 85, this.canvasSize.h / 2 + 65, 20, 20)
-        let coin14 = new Coins(this.ctx, this.canvasSize.w / 2 - 35, this.canvasSize.h / 2 + 65, 20, 20)
-        let coin15 = new Coins(this.ctx, this.canvasSize.w / 2 + 15, this.canvasSize.h / 2 + 65, 20, 20)
-        let coin16 = new Coins(this.ctx, this.canvasSize.w / 2 + 65, this.canvasSize.h / 2 + 65, 20, 20)
+        let coin13 = new Coins(this.ctx, this.canvasSize.w / 2 - 80, this.canvasSize.h / 2 + 70, 10, 10)
+        let coin14 = new Coins(this.ctx, this.canvasSize.w / 2 - 30, this.canvasSize.h / 2 + 70, 10, 10)
+        let coin15 = new Coins(this.ctx, this.canvasSize.w / 2 + 20, this.canvasSize.h / 2 + 70, 10, 10)
+        let coin16 = new Coins(this.ctx, this.canvasSize.w / 2 + 70, this.canvasSize.h / 2 + 70, 10, 10)
 
         this.coins.push(coin1, coin2, coin3, coin4, coin5, coin6, coin7, coin8, coin9, coin10, coin11, coin12, coin13, coin14, coin15, coin16)
     },
@@ -217,16 +276,17 @@ const OffTheLineClone = {
 
         this.coins.forEach(elm => {
             if (this.hero.heroPos.x < elm.coinPos.x + elm.coinSize.w &&
-            this.hero.heroPos.x + this.hero.heroSize.w > elm.coinPos.x &&
-            this.hero.heroPos.y < elm.coinPos.y + elm.coinSize.h &&
-            this.hero.heroPos.y + this.hero.heroSize.h > elm.coinPos.y) {
+                this.hero.heroPos.x + this.hero.heroSize.w > elm.coinPos.x &&
+                this.hero.heroPos.y < elm.coinPos.y + elm.coinSize.h &&
+                this.hero.heroPos.y + this.hero.heroSize.h > elm.coinPos.y) {
                 let index = this.coins.indexOf(elm)
-                if(index !== -1){
-                   this.coins.splice(index, 1)
+                if (index !== -1) {
+                    this.coins.splice(index, 1)
+                    this.score++
                 }
-            } 
+            }
         })
-},
+    },
 
     setListener() {
         document.onkeypress = e => {
@@ -294,25 +354,25 @@ const OffTheLineClone = {
         //         this.hero.heroPos.x + 20 > this.canvasSize.w / 2 + (num) &&
         //         this.hero.heroPos.y < this.canvasSize.h / 2 - 80 + 10 &&
         //         this.hero.heroPos.y + 20 > this.canvasSize.h / 2 - 80) {
-                
+
         //     }
         //     if (this.hero.heroPos.x < this.canvasSize.w / 2 + (num) + 10 &&
         //         this.hero.heroPos.x + 20 > this.canvasSize.w / 2 + (num) &&
         //         this.hero.heroPos.y < this.canvasSize.h / 2 - 30 + 10 &&
         //         this.hero.heroPos.y + 20 > this.canvasSize.h / 2 - 30) {
-                
+
         //     }
         //     if (this.hero.heroPos.x < this.canvasSize.w / 2 + (num) + 10 &&
         //         this.hero.heroPos.x + 20 > this.canvasSize.w / 2 + (num) &&
         //         this.hero.heroPos.y < this.canvasSize.h / 2 + 20 + 10 &&
         //         this.hero.heroPos.y + 20 > this.canvasSize.h / 2 + 20) {
-                
+
         //     }
         //     if (this.hero.heroPos.x < this.canvasSize.w / 2 + (num) + 10 &&
         //         this.hero.heroPos.x + 20 > this.canvasSize.w / 2 + (num) &&
         //         this.hero.heroPos.y < this.canvasSize.h / 2 + 70 + 10 &&
         //         this.hero.heroPos.y + 20 > this.canvasSize.h / 2 + 70) {
-                
+
         //     }
         //     num += 50
         // }
--- js/hero.js
+++ js/hero.js
@@ -1,9 +1,9 @@
 class Hero {
-    constructor(canvasCtx, canvasSize, guideLine, heroWidth, heroHeight, speed) {
+    constructor(canvasCtx, canvasSize, limitSquare, heroWidth, heroHeight, speed) {
         this.ctx = canvasCtx
         this.canvasSize = canvasSize
         this.heroPos = {
-            x: this.canvasSize.w / 2 - 135,
+            x: this.canvasSize.w / 2 - 134,
             y: this.canvasSize.h / 2 - 135
         }
         this.heroSize = {
@@ -13,10 +13,9 @@ class Hero {
 
         this.speed = speed
 
-        this.guideLine = guideLine
-
         this.direction = ''
 
+        this.limitSquare = limitSquare
 
     }
 
@@ -30,52 +29,34 @@ class Hero {
 
     moveHero() {
 
-        // if (this.heroPos.x >= this.canvasSize.w / 2 - 135 && this.heroPos.x < this.canvasSize.w / 2 + 115) {
-        //     this.direction = 'top'
-        //     this.changeDirection("right")
-        //     console.log(this.direction)
-        // } else if (this.heroPos.x > this.canvasSize.w / 2 && this.heroPos.y < this.canvasSize.h / 2 + 115) {
-        //     this.direction = 'right'
-        //     this.changeDirection("down")
-        //     console.log(this.direction)
-        // } else if (this.heroPos.x <= this.canvasSize.w / 2 + 115 && this.heroPos.x > this.canvasSize.w / 2 - 135) {
-        //     this.direction = 'bottom'
-        //     this.changeDirection("left")
-        //     console.log(this.direction)
-        // } else if (this.heroPos.x < this.canvasSize.w / 2 && this.heroPos.y >= this.canvasSize.h / 2 - 135) {
-        //     this.direction = 'left'
-        //     this.changeDirection("up")
-        //     console.log(this.direction)
-        // } else {
-        //     this.changeDirection("left")
-        // }
-        if (this.heroPos.x >= this.canvasSize.w / 2 - 135 && this.heroPos.x < this.canvasSize.w / 2 + 115) {
-            this.direction = 'top'
-            this.changeDirection("right")
-          
-        } else if (this.heroPos.x > this.canvasSize.w / 2 && this.heroPos.y < this.canvasSize.h / 2 + 115) {
-            this.direction = 'right'
-            this.changeDirection("down")
-           
-        } else if (this.heroPos.x <= this.canvasSize.w / 2 + 115 && this.heroPos.x > this.canvasSize.w / 2 - 135) {
-            this.direction = 'bottom'
-            this.changeDirection("left")
-            
-        } else if (this.heroPos.x < this.canvasSize.w / 2 && this.heroPos.y >= this.canvasSize.h / 2 - 135) {
-            this.direction = 'left'
-            this.changeDirection("up")
-           
-        } else {
-            this.changeDirection("left")
+        // if (this.heroPos.x >= this.limitSquare.pos.x &&
+        //     this.heroPos.x + this.heroSize.w <= this.limitSquare.pos.x + this.limitSquare.size.w &&
+        //     this.heroPos.y + this.heroSize.h <= this.limitSquare.pos.y + this.limitSquare.size.h &&
+        //     this.heroPos.y >= this.limitSquare.pos.y) {
+        if (this.heroPos.y >= this.limitSquare.pos.y && this.heroPos.x + this.heroSize.w < this.limitSquare.pos.x + this.limitSquare.size.w) {
+            this.changeDirection('right')
         }
+        else if (this.heroPos.x + this.heroSize.w === this.limitSquare.pos.x + this.limitSquare.size.w && this.heroPos.y + this.heroSize.h < this.limitSquare.pos.y + this.limitSquare.size.h) {
+            this.changeDirection('down')
+        }
+        else if (this.heroPos.x <= this.limitSquare.pos.x + this.limitSquare.size.w && this.heroPos.x > this.limitSquare.pos.x) {
+            this.changeDirection('left')
+        }
+        else {
+            if (this.heroPos.x >= this.limitSquare.pos.x) {
+                alert('FUNCIONA!')
+            }
+            this.changeDirection('up')
+        }
+        // }
 
 
+        console.log(this.heroPos.y + this.heroSize.h)
+        console.log(this.limitSquare.pos.y + this.limitSquare.size.h)
+        console.log(this.heroPos.x)
+        console.log(this.limitSquare.pos.x)
 
-        // console.log(this.heroPos.y)
-        // console.log((this.canvasSize.h / 2 + - 135))
-        // console.log(this.heroPos.x)
-        // console.log(this.canvasSize.w / 2 + 115)
-        // console.log(this.speed)
+
 
     }
 
@@ -141,3 +122,53 @@ class Hero {
         // } else {
         //     this.changeDirection("left")
         // }h
+
+       // < --------------------------->
+            // if (this.heroPos.x >= this.canvasSize.w / 2 - 135 && this.heroPos.x < this.canvasSize.w / 2 + 115) {
+        //     this.direction = 'top'
+        //     this.changeDirection("right")
+        //     console.log(this.direction)
+        // } else if (this.heroPos.x > this.canvasSize.w / 2 && this.heroPos.y < this.canvasSize.h / 2 + 115) {
+        //     this.direction = 'right'
+        //     this.changeDirection("down")
+        //     console.log(this.direction)
+        // } else if (this.heroPos.x <= this.canvasSize.w / 2 + 115 && this.heroPos.x > this.canvasSize.w / 2 - 135) {
+        //     this.direction = 'bottom'
+        //     this.changeDirection("left")
+        //     console.log(this.direction)
+        // } else if (this.heroPos.x < this.canvasSize.w / 2 && this.heroPos.y >= this.canvasSize.h / 2 - 135) {
+        //     this.direction = 'left'
+        //     this.changeDirection("up")
+        //     console.log(this.direction)
+        // } else {
+        //     this.changeDirection("left")
+        // }
+
+        //<-------------------------------->
+         // if (this.heroPos.x >= this.canvasSize.w / 2 - 135 && this.heroPos.x < this.canvasSize.w / 2 + 115) {
+        //     this.direction = 'top'
+        //     this.changeDirection("right")
+
+        // } else if (this.heroPos.x > this.canvasSize.w / 2 && this.heroPos.y < this.canvasSize.h / 2 + 115) {
+        //     this.direction = 'right'
+        //     this.changeDirection("down")
+
+        // } else if (this.heroPos.x <= this.canvasSize.w / 2 + 115 && this.heroPos.x > this.canvasSize.w / 2 - 135) {
+        //     this.direction = 'bottom'
+        //     this.changeDirection("left")
+
+        // } else if (this.heroPos.x < this.canvasSize.w / 2 && this.heroPos.y >= this.canvasSize.h / 2 - 135) {
+        //     this.direction = 'left'
+        //     this.changeDirection("up")
+
+        // } else {
+        //     this.changeDirection("left")
+        // }
+
+
+
+        // console.log(this.heroPos.y)
+        // console.log((this.canvasSize.h / 2 + - 135))
+        // console.log(this.heroPos.x)
+        // console.log(this.canvasSize.w / 2 + 115)
+        // console.log(this.speed)
